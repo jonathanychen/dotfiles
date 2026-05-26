143f1dd98cb9c3f384da1c4fd14248535ed60b2b
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2026-05-26 00:24:01 -0700

    nvim

---
+++ .config/nvim/lua/jonathanychen/keymap.lua
@@ -0,0 +1,58 @@
+vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
+vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
+vim.keymap.set('n', 'grr', vim.lsp.buf.references)
+vim.keymap.set('n', 'gr?', vim.diagnostic.open_float)
+vim.keymap.set('n', 'grd', vim.lsp.buf.definition)
+
+local telescope_builtin = require('telescope.builtin')
+
+vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>", { desc = 'Telescope browse files' })
+vim.keymap.set("n", "<space>ff", telescope_builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set("n", "<space>fg", telescope_builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set("n", "<space>fh", telescope_builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Move within visual lines
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Harpoon
+local harpoon = require('harpoon')
+
+harpoon:setup()
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
+
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+-- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+-- Barbar
+vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>")
+vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>")
+vim.keymap.set("n", "<A-x>", "<Cmd>BufferClose<CR>")
---
+++ .config/nvim/lua/jonathanychen/lazy/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = {
+                    "javascript", "typescript", "lua", "go", "python", "cpp", "haskell", "tsx"
+                },
+
+                sync_install = false,
+
+                indent = {
+                    enable = true,
+                },
+
+                highlight = {
+                    enable = true,
+                },
+            })
+
+            require('nvim-treesitter.parsers')
+        end
+    }
+}
